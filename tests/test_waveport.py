9b58ac466d11f2ec5af1beaf8b657588bc5837c9
phelyx42 <44293568+phelyx42@users.noreply.github.com>
2020-10-28 17:20:12 +0100

    revisited terminals

--- tests/test_waveport.py
+++ tests/test_waveport.py
@@ -38,12 +38,6 @@ MSL = rect(chip_body,
            name="MSL")
 MSL.assign_perfect_E('_perfE')
 
-GND = rect(chip_body,
-           [-width/2, 0, -sub_h],
-           [width, MSL_length, 0], 
-           name="GND")
-GND.assign_perfect_E('_perfE')
-
 # define vacuum
 cover = box(chip_body,
                 [-width/2, 0, 0],
@@ -57,11 +51,11 @@ port1 = rect(chip_body,
               [width, 0, cover_H+sub_h],
               name="1")
 port1.assign_waveport(Nmodes=1)
-port1.assign_terminal_auto(GND)
+port1.assign_terminal_auto(MSL)
 
 port2 = rect(chip_body,
               [-width/2, MSL_length, -sub_h], 
               [width, 0, cover_H+sub_h],
               name="2")
 port2.assign_waveport(Nmodes=1)
-port2.assign_terminal_auto(GND)
+port2.assign_terminal_auto(MSL)
