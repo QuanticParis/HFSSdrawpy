1f184be5834a4e0bf6c7c07736770d9d959268b5
Adrien Bocquet <adrien.bocquet@alice-bob.com>
2022-06-07 11:18:41 +0200

    Add documentation to symmetries

--- tests/test_symmetry.py
+++ tests/test_symmetry.py
@@ -1,7 +1,6 @@
 import numpy as np
 
 from HFSSdrawpy import Modeler, Body
-from HFSSdrawpy.path_finding.path_finder import Path
 
 pm = Modeler("gds")
 body = Body(pm, "chip")
@@ -13,26 +12,18 @@ track = pm.set_variable("100mm")
 gap = pm.set_variable("50mm")
 fillet = track + 1.5 * gap
 
-with body([0, 0], [1, 0]):
-    body.polyline([[-dx, -dy], [-dx, dy], [dx, dy], [dx, -dy]])
-    mirror = body.mirror(np.pi / 2, 0)
-    with mirror:
-        body.polyline(
-            [[0, track], [track, 2 * track], [3 * track, 2 * track]], closed=False
-        )
-        with body([0, 0.5], [1, 0]):
-            with body([-1, 0.6], [1, 0]):
-                (port_1,) = body.port(name="port_1", widths=[track, track + 2 * gap])
-            # with body([0, 0], [0, -1]):
-            #     (port_2,) = body.port(name="port_2", widths=None)
-            with body([0, 0.1], [0, -1]):
-                (port_3,) = body.port(name="port_3", widths=[track, track + 2 * gap])
-        # path = Path("my_path", port_1, port_2, fillet)
-        # body.path(path.points, port_3.r, fillet=fillet)
-        body.draw_cable(port_1, port_3, fillet=fillet)
+body.polyline([[-dx, -dy], [-dx, dy], [dx, dy], [dx, -dy]], layer=3)
+mirror = body.mirror(np.pi / 2, 0)
+with mirror:
+    # body.polyline([[0, track], [track, 2 * track], [3 * track, 2 * track]], closed=True)
+    with body([-10 * track, 10 * track], [1, 0]):
+        (port_1,) = body.port(name="port_1", widths=[track, track + 2 * gap])
+    with body([0, 2 * track], [0, -1]):
+        (port_2,) = body.port(name="port_2", widths=[track, track + 2 * gap])
+    body.draw_cable(port_1, port_2, fillet=fillet)
 
-    port_3_mirror = mirror.get_symmetric_of(port_3)
-body.draw_cable_auto(port_3, port_3_mirror.r, fillet=fillet)
+port_2_symmetric = mirror.get_symmetric_of(port_2)
+body.draw_cable_auto(port_2, port_2_symmetric.r, fillet=fillet)
 
 path = r"."
 pm.generate_gds(path, "test_symmetry")
